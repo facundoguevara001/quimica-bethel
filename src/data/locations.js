c7f056002e8c8cf4ba22f8456978f71b8f6783e9
Facundo Guevara <facundoguevara001@gmail.com>
2026-08-29 13:09:10 -0300

    Actualización

--- src/data/locations.js
+++ src/data/locations.js
@@ -4,20 +4,20 @@ const locations = [
     {
         id: "sucursal-1",
         name: "Sucursal 1",
-        address: "Dirección a confirmar, Palermo",
-        mapQuery: "Palermo, Buenos Aires, Argentina",
+        address: "Iguazú 9030, B1657AHR Loma Hermosa, Provincia de Buenos Aires",
+        mapQuery: "Iguazú 9030, B1657AHR Loma Hermosa, Provincia de Buenos Aires",
     },
     {
         id: "sucursal-2",
         name: "Sucursal 2",
-        address: "Dirección a confirmar, Caballito",
-        mapQuery: "Caballito, Buenos Aires, Argentina",
+        address: "Av. Boedo 795, C1218 Cdad. Autónoma de Buenos Aires",
+        mapQuery: "Av. Boedo 795, C1218 Cdad. Autónoma de Buenos Aires",
     },
     {
         id: "sucursal-3",
         name: "Sucursal 3",
-        address: "Dirección a confirmar, Belgrano",
-        mapQuery: "Belgrano, Buenos Aires, Argentina",
+        address: "Beazley 1155, B1674AVQ Sáenz Peña, Provincia de Buenos Aires",
+        mapQuery: "Beazley 1155, B1674AVQ Sáenz Peña, Provincia de Buenos Aires",
     },
 ];
 
